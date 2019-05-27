625b43b9c435415fb034611352360063b02d7e70
Elmo@gremlim (from Travis CI) <deploy@travis-ci.org>
2019-05-27 04:09:39 +0000

    Deploy ingeekar/badge-app to github.com/ingeekar/badge-app.git:master

---
+++ precache-manifest.672cfef5701b6e2ce2ca079943a01754.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963053082386180947ef4f646d3b0a41",
+    "url": "/badge-app/index.html"
+  },
+  {
+    "revision": "eda95caff8d1d18e0dab",
+    "url": "/badge-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5cd479737cb03d86665e",
+    "url": "/badge-app/static/css/main.9f8e4c86.chunk.css"
+  },
+  {
+    "revision": "eda95caff8d1d18e0dab",
+    "url": "/badge-app/static/js/2.3a351480.chunk.js"
+  },
+  {
+    "revision": "5cd479737cb03d86665e",
+    "url": "/badge-app/static/js/main.1f1dc425.chunk.js"
+  },
+  {
+    "revision": "ab15561d062e10ddf63a",
+    "url": "/badge-app/static/js/runtime~main.9ef9ebd2.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/badge-app/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/badge-app/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/badge-app/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/badge-app/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/badge-app/static/media/stars.1d17360a.svg"
+  }
+]);
