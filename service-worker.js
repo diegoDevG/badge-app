e05945b33d732a6d1f9bdd83ac34858d88299516
Elmo@gremlim (from Travis CI) <deploy@travis-ci.org>
2019-05-27 16:20:12 +0000

    Deploy ingeekar/badge-app to github.com/ingeekar/badge-app.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/badge-app/precache-manifest.672cfef5701b6e2ce2ca079943a01754.js"
+  "/precache-manifest.8d92f03474126c991fe985b6d23206b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/badge-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
